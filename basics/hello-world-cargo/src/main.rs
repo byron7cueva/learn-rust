104ed68e16eb3b30f61f47aedeaed1ef0bf4573f
byron7cueva <byron7cueva@gmail.com>
2020-06-17 17:09:07 -0500

    Function

--- basics/hello-world-cargo/src/main.rs
+++ basics/hello-world-cargo/src/main.rs
@@ -1,26 +1,17 @@
-// Tuples
-
 fn main () {
-  // Define a tuple
-  let tup1 = (10, 20, 30, 40);
-
-  // Access to value of position
-  // Access to 3th position. Initialice from 0
-  println!("{}", tup1.2);
-
-  let tup2 = (20, "Rust", 3.4, false);
-  println!("{}", tup2.3);
-
-  //A tuple into other tuple
-  let tup3 = (20, "Rust", 3.4, false, (1, 4, 7));
-  println!("{}", (tup3.4).1);
-
-  //
-  let tup4 = (45, 6.7, "Computer", 56);
-  let (a, b, c, d) = tup4;
-
-  println!("a is {}", a);
-  println!("b is {}", b);
-  println!("c is {}", c);
-  println!("d is {}", d);
+  print_numbers_to(10);
 }
+
+fn print_numbers_to (num: u32) { // This function receibe a number unsign of 32 bits
+  for n in 1..num {
+    if is_even(n) {
+      println!("{} is even", n);
+    } else {
+      println!("{} is odd", n);
+    }
+  }
+}
+
+fn is_even (num: u32) -> bool { // This function return to boolean
+  return num % 2 == 0;
+}  
