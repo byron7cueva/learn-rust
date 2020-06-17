48510775248a2ac01a7230e35206cde061a4f3e1
byron7cueva <byron7cueva@gmail.com>
2020-06-17 16:57:13 -0500

    Tuples

---
+++ basics/hello-world-cargo/src/const.rs
@@ -0,0 +1,8 @@
+// Constants
+const MAXIMUN_NUMBER: u8 = 20;
+
+fn main () {
+  for n in 1..MAXIMUN_NUMBER {
+    println!("{}", n);
+  }
+}
--- basics/hello-world-cargo/src/main.rs
+++ basics/hello-world-cargo/src/main.rs
@@ -1,8 +1,26 @@
-// Constants
-const MAXIMUN_NUMBER: u8 = 20;
+// Tuples
 
 fn main () {
-  for n in 1..MAXIMUN_NUMBER {
-    println!("{}", n);
-  }
+  // Define a tuple
+  let tup1 = (10, 20, 30, 40);
+
+  // Access to value of position
+  // Access to 3th position. Initialice from 0
+  println!("{}", tup1.2);
+
+  let tup2 = (20, "Rust", 3.4, false);
+  println!("{}", tup2.3);
+
+  //A tuple into other tuple
+  let tup3 = (20, "Rust", 3.4, false, (1, 4, 7));
+  println!("{}", (tup3.4).1);
+
+  //
+  let tup4 = (45, 6.7, "Computer", 56);
+  let (a, b, c, d) = tup4;
+
+  println!("a is {}", a);
+  println!("b is {}", b);
+  println!("c is {}", c);
+  println!("d is {}", d);
 }
